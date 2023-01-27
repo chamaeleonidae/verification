3b55f286a524eb07ab265d1446989bd46ef4fbfd
shaggun <shaggynetworks@gmail.com>
2023-01-27 14:08:07 -0600

    Remove extra parenthesis

--- verification.py
+++ verification.py
@@ -7,7 +7,7 @@ import hashlib
 
 secret = os.getenv('CHAMELEON_VERIFICATION_SECRET')
 now = string(int(time.time()))
-uid_hash = '-'.join([hmac.new(bytes(secret, 'utf-8'), msg = bytes('-'.join([uid, now]), 'utf-8'), digestmod = hashlib.sha256).hexdigest()), now])
+uid_hash = '-'.join([hmac.new(bytes(secret, 'utf-8'), msg = bytes('-'.join([uid, now]), 'utf-8'), digestmod = hashlib.sha256).hexdigest(), now])
 
 #
 # send back the `uid_hash` along side `uid` and add to the existing `chmln.identify(uid, { uid_hash: uid_hash })`
